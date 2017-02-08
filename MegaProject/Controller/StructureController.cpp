ec243598de178dcdb1bc660ba99178b44e99b18c
Sparticus403 <twil8980@740s-pl-118.local>
2017-02-08 09:27:47 -0700

    created controller files

---
+++ MegaProject/Controller/StructureController.cpp
@@ -0,0 +1,24 @@
+//
+//  StructureController.cpp
+//  MegaProject
+//
+//  Created by Williamson, Todd on 2/8/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include "StructureController.hpp"
+
+StructureController :: StructureController()
+{
+    
+}
+
+void StructureController :: testIntArray()
+{
+    
+}
+
+void StructureController :: start()
+{
+    
+}
