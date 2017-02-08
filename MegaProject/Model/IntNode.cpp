d600f96674e954ca3571cbd3300d45705b989428
Sparticus403 <twil8980@740s-pl-118.local>
2017-02-08 08:17:15 -0700

    finished methods

--- MegaProject/Model/IntNode.cpp
+++ MegaProject/Model/IntNode.cpp
@@ -10,35 +10,38 @@
 
 IntNode :: IntNode()
 {
-    
+    this->nodeData = -999;
+    this->nodePointer = nullptr;
 }
 
-IntNode :: IntNode(int value)
+IntNode :: IntNode(int data)
 {
-    
+    this->nodeData = data;
+    this->nodePointer = nullptr;
 }
 
-IntNode :: IntNode(int value, IntNode * nextNode)
+IntNode :: IntNode(int nodeData, IntNode * next)
 {
-    
+    this->nodeData = nodeData;
+    this->nodePointer = next;
 }
 
 int IntNode :: getNodeData()
 {
-    
+    return nodeData;
 }
 
 IntNode * IntNode :: getNodePointer()
 {
-    
+    return nodePointer;
 }
 
-void IntNode :: setNodeData(int value)
+void IntNode :: setNodeData(int nodeData)
 {
-    
+    this->nodeData = nodeData;
 }
 
-void IntNode :: setNodePointer(IntNode * nextNode)
+void IntNode :: setNodePointer(IntNode * next)
 {
-    
+    this->nodePointer = next;
 }
