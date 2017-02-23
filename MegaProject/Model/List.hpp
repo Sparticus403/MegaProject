c27476d92613863c402ceffbd5d36fe63faabfb5
Sparticus403 <twil8980@740s-pl-118.local>
2017-02-23 08:36:36 -0700

    made constructor for the list template

--- MegaProject/Model/List.hpp
+++ MegaProject/Model/List.hpp
@@ -25,14 +25,46 @@ public:
     ~List<Type>();
     
     void addAtIndex(int index, Type value);
-    void add(Type value);
+    void addFront(Type value);
+    void addEnd(Type value);
     Type remove(int index);
     Type setAtIndex(int index, Type data);
     Type getFromIndex(int index);
     bool contains(Type data);
     int getSize() const;
     Node<Type> * getFront() const;
+    Node<Type> * getEnd() const;
 };
+
+template <class Type>
+List<Type> :: List()
+{
+    this->size = 0;
+    this->front = nullptr;
+    this->end = nullptr;
+}
+
+template <class Type>
+void List<Type> :: addFront(Type value)
+{
+    if(size == 0)
+    {
+        Node<Type> * first = new Node<Type>(value);
+        this->front = first;
+        this->end = first;
+    }
+    else
+    {
+        Node<Type> * newFirst = new Node<Type>();
+        newFirst->setNodeData(value);
+        newFirst->setNodePointer(front);
+        //or
+        //Node<Type> * newFirst = new Node<Type>(value, front);
+        front = newFirst;
+    }
+    
+    size++;
+}
 
 
 
