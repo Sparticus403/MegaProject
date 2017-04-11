7e327b40d4af87354c59b10bfb48438fdf87c823
Sparticus403 <twil8980@740s-pl-118.local>
2017-04-11 09:25:06 -0600

    finished declaring all elements in BinarySearchTree class

--- MegaProject/Model/BinarySearchTreeNode.hpp
+++ MegaProject/Model/BinarySearchTreeNode.hpp
@@ -30,4 +30,55 @@ public:
     void setRightChild(BinarySearchTreeNode<Type> * right);
 };
 
+template <class Type>
+BinarySearchTreeNode :: BinarySearchTreeNode()
+{
+    this->root = nullptr;
+    this->rightChild = nullptr;
+    this->leftChild = nullptr;
+}
+
+template <class Type>
+BinarySearchTreeNode :: BinarySearchTreeNode(Type data) : Node<Type>(data)
+{
+    this->root = nullptr;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * BinarySearchTreeNode<Type> :: getRootPointer()
+{
+    return this->root;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * BinarySearchTreeNode<Type> :: getleftChild()
+{
+    return this->leftChild;
+}
+
+template <class Type>
+BinarySearchTreeNode<Type> * BinarySearchTreeNode<Type> :: getRightChild()
+{
+    return this->rightChild;
+}
+
+template <class Type>
+void BinarySearchTreeNode<Type> :: setRootPointer(BinarySearchTreeNode<Type>* root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void BinarySearchTreeNode<Type> :: setRootPointer(BinarySearchTreeNode<Type>* left)
+{
+    this->leftChild = left;
+}
+
+template <class Type>
+void BinarySearchTreeNode<Type> :: setRootPointer(BinarySearchTreeNode<Type>* right)
+{
+    this->rightChild = right;
+}
 #endif /* BinarySearchTreeNode_h */
