d25a6895a4f81ebd24e598e82cd64399e947056c
Sparticus403 <twil8980@740s-pl-118.local>
2017-04-11 08:34:22 -0600

    made BinarySearchTreeNode class

---
+++ MegaProject/Model/BinarySearchTreeNode.hpp
@@ -0,0 +1,33 @@
+//
+//  BinarySearchTreeNode.hpp
+//  MegaProject
+//
+//  Created by Williamson, Todd on 4/11/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#ifndef BinarySearchTreeNode_h
+#define BinarySearchTreeNode_h
+
+template <class Type>
+class BinarySearchTreeNode : public Node<Type>
+{
+private:
+    BinarySearchTreeNode<Type> * root;
+    BinarySearchTreeNode<Type> * leftChild;
+    BinarySearchTreeNode<Type> * rightChild;
+    
+public:
+    BinarySearchTreeNode();
+    BinarySearchTreeNode(Type data);
+    
+    BinarySearchTreeNode<Type> * getRootPointer();
+    BinarySearchTreeNode<Type> * getLeftChild();
+    BinarySearchTreeNode<Type> * getRightChild();
+    
+    void setRootPointer(BinarySearchTreeNode<Type> * root);
+    void setLeftChild(BinarySearchTreeNode<Type> * left);
+    void setRightChild(BinarySearchTreeNode<Type> * right);
+};
+
+#endif /* BinarySearchTreeNode_h */
---
+++ MegaProject/Model/BinaryTreeNode.hpp
@@ -0,0 +1,13 @@
+//
+//  BinaryTreeNode.hpp
+//  MegaProject
+//
+//  Created by Williamson, Todd on 4/11/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_h
+#define BinaryTreeNode_h
+
+
+#endif /* BinaryTreeNode_h */
